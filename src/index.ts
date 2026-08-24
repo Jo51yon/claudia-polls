1e9e146d3931c20fb39f2fb0d162625ed26b972c
Jo51yon <jolyon.grace@gracenet.co.uk>
2026-08-24 17:55:23 +0100

    initial: src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { default as ClaudiaPollView } from './ClaudiaPollView';
+export type { ClaudiaPollViewProps, ClaudiaPollViewCopy } from './ClaudiaPollView';
+export { default as ClaudiaPollResults } from './ClaudiaPollResults';
+export type { ClaudiaPollResultsProps, ClaudiaPollResultsCopy } from './ClaudiaPollResults';
+export type {
+  ClaudiaPoll, ClaudiaPollQuestion, ClaudiaPollOption, ClaudiaPollResultRow,
+  ClaudiaPollQuestionType, ClaudiaPollResultsVisibility,
+} from './types';
